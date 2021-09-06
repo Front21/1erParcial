16b06ce3c8d5dfcfbfa9bded5292166445122b15
Kathia98 <kathiamaria.kg@gmail.com>
2021-09-06 18:29:34 -0400

    Implementacion login

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+// app.module.ts
+
+import { routing } from "./app.routing";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, routing,FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+  
+})
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+
+// login.component.ts
+
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent  {
+  email: string = '';
+  password: string = '';
+
+  /**constructor(email: string, password: string) {
+    this.email = email,
+    this.password = password
+  }*/
+
+  login() {
+    console.log(this.email);
+    console.log(this.password);
+  }
+}
