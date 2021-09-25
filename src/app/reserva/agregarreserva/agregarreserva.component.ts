66f12c2b5cc2443da46decf21327e1f3971b9b5e
PedroRamosGenes <pedro.genes98@fpuna.edu.py>
2021-09-25 01:12:39 -0400

    Agregar reserva funcionando correctamente.

--- src/app/reserva/agregarreserva/agregarreserva.component.ts
+++ src/app/reserva/agregarreserva/agregarreserva.component.ts
@@ -7,6 +7,8 @@ import { ServiceclienteService } from 'src/app/service/servicecliente.service';
 import { ServiceempleadoService } from 'src/app/service/serviceempleado.service';
 import { ServicereservaService } from 'src/app/service/servicereserva.service';
 import {FormsModule, NgForm} from '@angular/forms';
+import {Agenda} from "../../model/agenda";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -19,12 +21,11 @@ export class AgregarreservaComponent implements OnInit {
   reservas: Reserva[]=[];
   reservaResultado: Reserva[]=[];
   empleados: Persona[] =[];
-  empleaadoSelec: Persona = new Persona();
   clientes: Persona[] =[];
   clienteSelec: Persona = new Persona();
-  nuevaReserva: Reserva = new Reserva(); 
+  nuevaReserva: Reserva = new Reserva();
   idReservaSelec: number=0;
-  fechaSelec: string="";
+  fechaSelec: any;
   horaInicioSelec: string="";
   horaFinSelec: string="";
   fechaHoraCreacionSelec: string ="";
@@ -58,12 +59,27 @@ export class AgregarreservaComponent implements OnInit {
   empleadoSelec: Persona = new Persona();
   listar: string="";
   asistioSelec: string ="";
+  id: number = 0;
+  fecha: string = "";
+  agendas: Reserva[] = [];
+  horaIni: string[] = [];
+  horaFin: string[] = [];
+  clienteA: string[] = [];
+  agendaFinal: Agenda[] = [];
+  cont: number = 0;
+  clickBuscar: boolean = false;
+  mensajeBuscar: string = "";
+  agendaIdSelec: number = 0;
+  repetido: boolean = false;
 
 
   constructor(private servicioReserva : ServicereservaService, private servicioEmpleado: ServiceempleadoService,
-    private servicioCliente: ServiceclienteService) { }
+    private servicioCliente: ServiceclienteService,
+    private router: Router) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.id = 2; //xd
+    this.fecha = "20190905"; //xd
     this.servicioReserva.getReservas().subscribe(
       entity => this.reservas = entity.lista,
       error =>console.log('No se pudo acceder a la lista de Reservas')
@@ -79,25 +95,122 @@ export class AgregarreservaComponent implements OnInit {
       error =>console.log('No se pudo acceder a la lista de Clientes')
     );
 
+    //Disponible se utiliza para filtrar solo las agendas libres, false para filtras libres y ocupados
+    await this.filtro(false, this.fecha, this.id);
+
+
+
+  }
+
+  async filtro(noNecesitaDisponible: boolean, fecha: string, id: number): Promise<void>{
+    this.agendaFinal = [];
+    console.log(this.fecha);
+    if(!noNecesitaDisponible){
+      await this.servicioReserva.getAgendasConDisponible(id, fecha, "S").then(
+        entity => this.agendas = entity,
+        error =>console.log('No se pudo acceder a la lista de Reservas')
+      );
+    }else{
+      await this.servicioReserva.getAgendasSinDisponible(id, fecha).then(
+        entity => this.agendas = entity,
+        error =>console.log('No se pudo acceder a la lista de Reservas')
+      );
+
+    }
+    console.log('LENGTH agendas'+this.agendas.length);
+    this.cont = 0;
+    for(var agenda in this.agendas){
+      let agendaEntry: Agenda = new Agenda();
+      if(this.agendas[agenda].idReserva != null){
+        agendaEntry.idAgenda = this.cont;
+        agendaEntry.fechaCadena = this.agendas[agenda].fechaCadena;
+        agendaEntry.horaInicioCadena = this.agendas[agenda].horaInicioCadena;
+        agendaEntry.horaFinCadena = this.agendas[agenda].horaFinCadena;
+        agendaEntry.cliente = this.agendas[agenda].idCliente.nombre+' '+this.agendas[agenda].idCliente.apellido;
+      }else{
+        agendaEntry.idAgenda = this.cont;
+        agendaEntry.fechaCadena = this.agendas[agenda].fechaCadena;
+        agendaEntry.horaInicioCadena = this.agendas[agenda].horaInicioCadena;
+        agendaEntry.horaFinCadena = this.agendas[agenda].horaFinCadena;
+        agendaEntry.cliente = '-';
+      }
+      //Verificar que las horas no sean las mismas
+      this.repetido = false;
+      for(var agendaF in this.agendaFinal){
+        if(this.agendaFinal[agendaF].horaInicioCadena == agendaEntry.horaInicioCadena){
+          this.repetido = true;
+        }
+      }
+
+      if(!this.repetido){
+        this.agendaFinal.push(agendaEntry);
+      }
+      this.cont = this.cont + 1;
+    }
+    console.log('LENGTH agendaFinal'+this.agendaFinal.length);
+
+    if(this.agendaFinal.length == 0){
+      this.mensajeBuscar = "No existenresultados para esta busqueda."
+    }
+
   }
 
   public isChecked$ = new BehaviorSubject(false);
-        toggleChecked() {
-        this.isChecked$.next(!this.isChecked$.value)
-        console.log(this.isChecked$.value)
+  toggleChecked() {
+    this.isChecked$.next(!this.isChecked$.value)
+    console.log(this.isChecked$.value)
+    if(this.isChecked$.value){
+      //No es cuando cargo recien la pagina
+      if(this.clickBuscar){
+        this.buscarReservaenCrear(true);
+      }else{//Cargo recien la pagina
+        this.filtro(true, this.fecha, this.id);
+      }
+    }else{
+      if(this.clickBuscar){
+        this.buscarReservaenCrear(true);
+      }else{
+        this.filtro(false, this.fecha, this.id);
+      }
+    }
   }
- 
-  buscarReservaenCrear(): void{
-    
-    this.anod= this.fechadesde.toString().substr(0,4); 
-    this.mesd= this.fechadesde.toString().substr(5,2);
-    this.diad= this.fechadesde.toString().substr(8,2);
-    this.fechacadenad= this.anod+this.mesd+this.diad;
 
-    
+  buscarReservaenCrear(desdeCheck: boolean): void{
+    this.agendaFinal = [];
+    this.mensajeBuscar = "";
+    if(!desdeCheck){
+      this.clickBuscar = true;
+    }
+    if(this.fechaSelec != undefined && this.empleadoSelec.idPersona != undefined){
+      this.anod= this.fechaSelec.toString().substr(0,4);
+      this.mesd= this.fechaSelec.toString().substr(5,2);
+      this.diad= this.fechaSelec.toString().substr(8,2);
+      this.fechacadenad= this.anod+this.mesd+this.diad;
+
+      if(this.isChecked$.value){
+        this.filtro(true, this.fechacadenad, this.empleadoSelec.idPersona);
+      }else{
+        this.filtro(false, this.fechacadenad, this.empleadoSelec.idPersona);
+      }
+    }
+
+    if(this.fechaSelec != undefined && this.empleadoSelec.idPersona == undefined){
+      this.mensajeBuscar = "Por favor, marca un empleado.";
+    }
+
+    if(this.fechaSelec == undefined && this.empleadoSelec.idPersona != undefined){
+      this.mensajeBuscar = "Por favor, marca una fecha.";
+    }
+
+    if(this.fechaSelec == undefined && this.empleadoSelec.idPersona == undefined){
+      this.mensajeBuscar = "Por favor, marca una fecha y un empleado.";
+    }
+
+
+
    // this.servicioReserva.getReservaEmpleadosenCrear(this.empleadoSelec.idPersona,this.fechacadenad).subscribe(
      // entity => this.reservaResultado = entity.lista,
-      //error =>console.log('No se pudo acceder a la lista de Reservas por Empleados'), 
+      //error =>console.log('No se pudo acceder a la lista de Reservas por Empleados'),
     //);
 
 
@@ -106,31 +219,54 @@ export class AgregarreservaComponent implements OnInit {
 
  seleccionado(reser: Reserva): void{
    console.log(reser.idReserva)
-   
+
  }
- 
 
-  crearReserva(): void{
 
-    //this.nuevaReserva.idReserva = this.idReservaSelec;
-    this.nuevaReserva.fechaCadena = this.fechaSelec;
-    this.nuevaReserva.fechaDesdeCadena = this.fechadesdecadenad;
-    this.nuevaReserva.fechaHastaCadena = this.fechahastacadenaf;
-    this.nuevaReserva.horaInicioCadena = this.horainicadenaSelec;
-    this.nuevaReserva.horaFinCadena = this.horafincadenaSelec;
-    this.nuevaReserva.fechaHoraCreacion = this.fechaHoraCreacionSelec;
-    this.nuevaReserva.flagEstado = this.flagEstadoSelec;
-    this.nuevaReserva.flagAsistio = this.FlagAsistioSelec;
-    this.nuevaReserva.observacion = this.observacionSelec;
-    this.nuevaReserva.idFichaClinica = this.idFichaClinicaSelec;
-    
-  
-    //this.servicioReserva.postReservas({idReserva: this.nuevaReserva.idReserva, fecha: this.nuevaReserva.fechaCadena, fechaDesdeCadena: this.nuevaReserva.fechaDesdeCadena, 
-      //fechaHastaCadena: this.nuevaReserva.fechaHastaCadena, horaInicio: this.nuevaReserva.horaInicioCadena, horaFin: this.nuevaReserva.horaFinCadena, 
-      //fechaHoraCreacion: this.nuevaReserva.fechaHoraCreacion, flagEstado: this.nuevaReserva.flagEstado, 
-      //flagAsistio: this.nuevaReserva.flagAsistio, observacion: this.nuevaReserva.observacion, idFichaClinica: this.nuevaReserva.idFichaClinica}).subscribe(
-      //() => {this.mensaje='Agregado exitosamente'},error => console.log("error: "+error));
+  async crearReserva(): Promise<void>{
 
+    //Si se elige el usuario y la fecha default
+    if(!this.clickBuscar){
+      this.fechacadenad = this.fecha;
+      this.empleadoSelec.idPersona = this.id;
+    }
+
+    let agendaSelec = this.agendaFinal.find(agenda => agenda.idAgenda == this.agendaIdSelec);
+
+    //console.log('idAgenda: '+ agendaSelec.idAgenda);
+    if(agendaSelec){
+      console.log('fechaCadena: '+ this.fechacadenad);
+      console.log('horaIni: '+ agendaSelec.horaInicioCadena);
+      console.log('horaFin: '+ agendaSelec.horaFinCadena);
+      console.log('idEmple: '+ this.empleadoSelec.idPersona);
+      console.log('idClien: '+ this.clienteSelec.idPersona);
+      console.log('observacion: '+ this.observacionSelec);
+      console.log('idSelec: '+ this.agendaIdSelec);
+      console.log('idAgenda: '+ agendaSelec.idAgenda);
+
+      await this.servicioReserva.postReservas({
+        fechaCadena: this.fechacadenad,
+        horaInicioCadena: agendaSelec.horaInicioCadena,
+        horaFinCadena: agendaSelec.horaFinCadena,
+        idEmpleado: {
+          idPersona: this.empleadoSelec.idPersona
+        },
+        idCliente: {
+          idPersona: this.clienteSelec.idPersona
+        },
+        observacion: this.observacionSelec
+      }).then(
+        () => {this.mensaje='Agregado exitosamente'},error => console.log("error: "+error));
+
+
+    }
+
+
+    await this.irListadoReservas();
+
+  }
+  async irListadoReservas(): Promise<boolean>{
+    return this.router.navigateByUrl('reserva');
   }
 
 
--- src/app/service/servicereserva.service.ts
+++ src/app/service/servicereserva.service.ts
@@ -11,9 +11,9 @@ import { Reserva } from '../model/reserva';
 export class ServicereservaService {
 
   status: string="";
- 
+
   private api: string ="http://181.123.243.5:8080/stock-pwfe/reserva";
-  
+
   constructor(private http: HttpClient) {   }
 
   getReservas(): Observable<listadatos<Reserva>> {
@@ -48,7 +48,7 @@ export class ServicereservaService {
     const ejemplo = JSON.stringify(filtro)
     return this.http.get<listadatos<Reserva>>(this.api, {params:{ejemplo}}).toPromise();
   }
-  
+
   async getReservaFechaencrear(fd:string): Promise<listadatos<Reserva>> {
     const filtro = {
       fechaDesdeCadena: fd
@@ -56,7 +56,7 @@ export class ServicereservaService {
     const ejemplo = JSON.stringify(filtro)
     return this.http.get<listadatos<Reserva>>(this.api, {params:{ejemplo}}).toPromise();
   }
-  
+
   async getReservaFechas(fd:string, ff:string): Promise<listadatos<Reserva>> {
     const filtro = {
       fechaDesdeCadena: fd,
@@ -65,6 +65,18 @@ export class ServicereservaService {
     const ejemplo = JSON.stringify(filtro)
     return this.http.get<listadatos<Reserva>>(this.api, {params:{ejemplo}}).toPromise();
   }
+
+  async getAgendasConDisponible(id: number, f: string, d: string): Promise<Reserva[]> {
+    const fecha = f;
+    const disponible = d;
+    return this.http.get<Reserva[]>("http://181.123.243.5:8080/stock-pwfe/persona/"+id+"/agenda", {params:{fecha, disponible}}).toPromise();
+  }
+
+  async getAgendasSinDisponible(id: number, f: string): Promise<Reserva[]> {
+    const fecha = f;
+    return this.http.get<Reserva[]>("http://181.123.243.5:8080/stock-pwfe/persona/"+id+"/agenda", {params:{fecha}}).toPromise();
+  }
+
 
   headers = new HttpHeaders({ "Content-Type": "application/json" , "usuario": "usuario2" });
   async postReservas(body: any): Promise<Reserva>{
@@ -75,8 +87,8 @@ export class ServicereservaService {
       )
     ).toPromise();
   }
-  
- 
+
+
   async deleteReserva(idCat: number): Promise<{}>{
     return this.http.delete(this.api+'/'+idCat).toPromise();
   }
@@ -84,7 +96,7 @@ export class ServicereservaService {
   getReserva(id: number): Observable<Reserva>{
     return this.http.get<Reserva>(this.api+'/'+id);
   }
- 
+
   putReserva(body: any): Observable<Reserva>{
     return this.http.put<Reserva>(this.api, body,{headers: this.headers}).pipe(
       tap( // Log the result or error
@@ -123,5 +135,5 @@ export class ServicereservaService {
   }
 
 
- 
+
 }
